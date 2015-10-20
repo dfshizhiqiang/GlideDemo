6e20f6b0d419acec6555049339256b50c690b75e
Zech <dfshizhiqiang@126.com>
2015-10-20 09:56:40 +0800

    add load callback

--- app/src/main/java/com/imzhiqiang/glidedemo/MainActivity.java
+++ app/src/main/java/com/imzhiqiang/glidedemo/MainActivity.java
@@ -1,12 +1,16 @@
 package com.imzhiqiang.glidedemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.ImageViewTarget;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,9 +19,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView imageView = (ImageView) findViewById(R.id.image);
+        final ImageView imageView = (ImageView) findViewById(R.id.image);
 
-        Glide.with(this).load("https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superplus/img/logo_white.png").into(imageView);
+        Glide.with(this).load("https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superplus/img/logo_white.png").into(new ImageViewTarget<GlideDrawable>(imageView) {
+            @Override
+            protected void setResource(GlideDrawable resource) {
+                imageView.setImageDrawable(resource);
+            }
+
+            @Override
+            public void onLoadStarted(Drawable placeholder) {
+                super.onLoadStarted(placeholder);
+                //加载开始，设置占位 placeholder
+            }
+
+            @Override
+            public void onLoadFailed(Exception e, Drawable errorDrawable) {
+                super.onLoadFailed(e, errorDrawable);
+                //加载失败，设置错误drawable errorDrawable
+            }
+
+            @Override
+            public void onResourceReady(GlideDrawable resource, GlideAnimation<? super GlideDrawable> glideAnimation) {
+                super.onResourceReady(resource, glideAnimation);
+                //加载成功
+            }
+        });
 
     }
 
