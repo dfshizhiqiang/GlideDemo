d720bfc6269b43d69c6cbf6085ca55c6d99d2c66
Zech <dfshizhiqiang@126.com>
2016-03-04 17:13:17 +0800

    添加缩略图

--- app/src/main/java/com/imzhiqiang/glidedemo/posts/PostThreeActivity.java
+++ app/src/main/java/com/imzhiqiang/glidedemo/posts/PostThreeActivity.java
@@ -4,12 +4,12 @@ import android.os.Bundle;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.imzhiqiang.glidedemo.R;
 
 public class PostThreeActivity extends BaseActivity {
 
     private String url = "http://ww3.sinaimg.cn/bmiddle/005AUQMNjw1f1jxncznq8g30b4069kjl.gif";
+    private String thumbnailUrl = "http://img0.ph.126.net/ZRtqC7-0gXMd4Pk6pXifdQ==/6598146188600193111.jpg";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +21,7 @@ public class PostThreeActivity extends BaseActivity {
         Glide
                 .with(this)
                 .load(url)
-                .skipMemoryCache(true)
-                .diskCacheStrategy(DiskCacheStrategy.NONE)
-                .thumbnail(0.3F)
+                .thumbnail(Glide.with(this).load(thumbnailUrl))
                 .into(imageView);
     }
 }
